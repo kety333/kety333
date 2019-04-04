0bfb8a405fe9a5d4cc27dac748fe5b8c4e4eccb7
kety <ketyd333@gmail.com>
2019-04-04 13:36:04 +0300

    add  toto

--- src/components/ui/ElementsGridLayout.js
+++ src/components/ui/ElementsGridLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import GridLayout from "react-grid-layout";
 import PropTypes from "prop-types";
 import layoutService from "../../services/layoutService";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as layoutActions from "../../redux/actions/layotActions";
 
@@ -11,19 +11,22 @@ class ElementsGridLayout extends React.Component {
     width: PropTypes.number.isRequired,
     rowHeight: PropTypes.number.isRequired,
     layoutId: PropTypes.number.isRequired,
-    layoutsList:PropTypes.array.isRequired,
+    layoutsList: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
   };
 
   state = {
-    layout:[]
+    layout: []
   };
 
   componentDidMount() {
-    const layoutById = layoutService.getLayoutById(this.props.layoutsList,this.props.layoutId);
+    const layoutById = layoutService.getLayoutById(
+      this.props.layoutsList,
+      this.props.layoutId
+    );
     const layout = layoutById !== undefined ? layoutById.layout : [];
     this.setState({
-        layout: layout
+      layout: layout
     });
   }
 
@@ -33,49 +36,32 @@ class ElementsGridLayout extends React.Component {
   };
 
   onDrop = event => {
-      event.preventDefault();
+    event.preventDefault();
 
     const data = event.dataTransfer.getData("dragContent");
     const targetLayoutElementId = event.target.id;
     this.addDragedItem(data, targetLayoutElementId);
-      // event.target.appendChild(data);
-
-      // this.setState(state => {
-      //     const layouts = state.layout.map((item, j) => {
-      //         if (j === i) {
-      //             return item + 1;
-      //         } else {
-      //             return item;
-      //         }
-      //     });
-      //
-      //     return {
-      //         list,
-      //     };
-    // const layouts = { ...this.state, title: event.target.value };
-    // this.props.actions.updateLayout(this.state.course);
-    //   this.setState({ layouts });
-    //      this.setState({
-    //       ...this.state,
-    //       layouts
-    //   });
+    //TODO - update  redux storeand  state.
+    //  this.props.actions.updateLayout(this.state.layouts);
+    // this.setState(state => {
+    //    }
   };
 
-    addDragedItem=(data, targetLayoutElementId) =>{
-        let div = document.createElement("div");
-        div.classList.add("element-to-select");
-        div.style.marginLeft = "100px";
-        let textnode = document.createTextNode(data);
-        div.appendChild(textnode);
-        const targetNode = document.getElementById(targetLayoutElementId);
-     //   targetNode.style.paddingLeft = "40%";
-        while (targetNode.firstChild) {
-            targetNode.removeChild(targetNode.firstChild);
-        }
-        targetNode.appendChild(div);
+  addDragedItem = (data, targetLayoutElementId) => {
+    let div = document.createElement("div");
+    div.classList.add("element-to-select");
+    div.style.marginLeft = "100px";
+    let textnode = document.createTextNode(data);
+    div.appendChild(textnode);
+    const targetNode = document.getElementById(targetLayoutElementId);
+    //   targetNode.style.paddingLeft = "40%";
+    while (targetNode.firstChild) {
+      targetNode.removeChild(targetNode.firstChild);
     }
+    targetNode.appendChild(div);
+  };
 
-    render() {
+  render() {
     return (
       <GridLayout
         className="layout elements-grid-layout"
@@ -92,7 +78,11 @@ class ElementsGridLayout extends React.Component {
             onDrop={e => this.onDrop(e, "complete")}
             onDragOver={this.dragOver}
           >
-              {item.value ? <div className="element-to-select">item.value</div> :   item.i}
+            {item.value ? (
+              <div className="element-to-select">item.value</div>
+            ) : (
+              item.i
+            )}
           </div>
         ))}
       </GridLayout>
@@ -101,15 +91,18 @@ class ElementsGridLayout extends React.Component {
 }
 
 function mapActionsToProp(dispatch) {
-    return {
-        actions: bindActionCreators(layoutActions, dispatch)
-    };
+  return {
+    actions: bindActionCreators(layoutActions, dispatch)
+  };
 }
 
 function mapStateToProps(storageState) {
-    return {
-        layoutsList: storageState.layouts
-    };
+  return {
+    layoutsList: storageState.layouts
+  };
 }
 
-export default connect(mapStateToProps, mapActionsToProp)(ElementsGridLayout)
+export default connect(
+  mapStateToProps,
+  mapActionsToProp
+)(ElementsGridLayout);
